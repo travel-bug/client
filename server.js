07fdfe2b1e3ae7141405a324fcb2673654d42476
Clark Alms <clark.alms@gmail.com>
2019-08-01 21:32:19 -0500

    working on mobile responsive and added server.js for heroku deployment

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("client/build"));
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
