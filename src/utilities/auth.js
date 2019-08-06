e1a9228796a99da483412192f8de5f8466f985f8
Chris Burke <burke1791@gmail.com>
2019-08-06 17:31:37 -0500

    created starter files for the basic utility classes of the data layer

---
+++ src/utilities/auth.js
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+
+var AuthService = {};
+
+// anonymous IIFE to generate the AuthService object
+(function(obj) {
+  obj.sendSigninRequest = (params) => {
+
+  }
+
+  obj.sendSignoutRequest = () => {
+
+  }
+})(AuthService);
+
+export default AuthService;
---
+++ src/utilities/data.js
@@ -0,0 +1,10 @@
+import Axios from 'axios';
+
+var DataService = {};
+
+// anonymous IIFE to generate the DataService object
+(function(obj) {
+
+})(DataService);
+
+export default DataService
