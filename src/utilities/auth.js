31494f796d6300ea4a88d38dc4cc59fc23ac9c6b
Chris Burke <burke1791@gmail.com>
2019-08-06 20:13:40 -0500

    added basic authentication functions that console log the API responses

--- src/utilities/auth.js
+++ src/utilities/auth.js
@@ -1,15 +1,42 @@
-import Axios from 'axios';
+import axios from 'axios';
+import { API_POST } from './constants';
 
 var AuthService = {};
 
 // anonymous IIFE to generate the AuthService object
 (function(obj) {
-  obj.sendSigninRequest = (params) => {
 
+  obj.sendSignupRequest = (params) => {
+    let authParams = {
+      username: params.username,
+      password: params.password,
+      email: params.email
+    };
+    axios.post(API_POST.signup, authParams).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  obj.sendSigninRequest = (params) => {
+    let authParams = {
+      username: params.username,
+      password: params.password
+    };
+    axios.post(API_POST.login, authParams).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   obj.sendSignoutRequest = () => {
-
+    axios.post(API_POST.signout).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    });
   }
 })(AuthService);
 
