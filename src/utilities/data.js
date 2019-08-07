2ef72307a7ea1a1156001bb575f7796652bbabf8
Chris Burke <burke1791@gmail.com>
2019-08-06 20:24:24 -0500

    added function get hit the top posts endpoint

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -1,10 +1,18 @@
-import Axios from 'axios';
+import axios from 'axios';
+import { API_GET } from './constants';
 
 var DataService = {};
 
 // anonymous IIFE to generate the DataService object
 (function(obj) {
-
+  obj.getTopPosts = () => {
+    axios.get(API_GET.top_posts).then(response => {
+      console.log(response);
+      // set the top posts to a variable or coordinate with context
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 })(DataService);
 
 export default DataService
