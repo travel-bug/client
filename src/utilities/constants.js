8fce3726975b94fe4e6bfa64390da77165c38960
Chris Burke <burke1791@gmail.com>
2019-08-10 14:45:19 -0500

    removed the initial slashes in the api route constants

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,10 +1,10 @@
 export const API_POST = {
-  login: '/api/login',
-  signup: '/api/signup',
-  signout: '/api/signout',
-  new_post: '/api/new_post'
+  login: 'api/login',
+  signup: 'api/signup',
+  signout: 'api/signout',
+  new_post: 'api/new_post'
 }
 
 export const API_GET = {
-  top_posts: '/api/top_posts',
+  top_posts: 'api/top_posts',
 }
