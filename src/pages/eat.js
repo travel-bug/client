197c14bd9454a79f3c1d01a93f1236aa9f26ef30
Clark Alms <clark.alms@gmail.com>
2019-08-20 19:58:58 -0500

    commiting to pull master

--- src/pages/eat.js
+++ src/pages/eat.js
@@ -3,14 +3,27 @@ import Image from "../images/Project3_eat.jpg";
 import Jumbotron from "../components/Jumbotron/jumbotron";
 import NewPosts from "../components/NewPosts/newPosts";
 import DataService from "../utilities/data";
+import PubSub from "../utilities/pubsub";
+import { NOTIF } from "../utilities/constants";
 // import {Container, Col, Row} from "../components/Grid/grid";
 
 
 class Eat extends Component {
     state = {
-
+        newPosts: null
     }
 
+    componentDidMount() {
+        PubSub.subscribe(NOTIF.TOP_EAT_POSTS, this, this.getTopEatPosts)
+    }
+
+    componentWillUnmount() {
+        PubSub.unsubscribe(NOTIF.SIGN_IN, this)
+    }
+
+    getTopEatPosts = () => {
+        DataService.getTopEatPosts({})
+    }
 
     render(){
         return (
